2857d41b185b9fe37803dba78e60c812ef53db2b
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 20:15:45 +0200

    init

---
+++ storm-console-logging/project.clj
@@ -0,0 +1,12 @@
+(def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
+(def VERSION (-> ROOT-DIR (str "/../VERSION") slurp (.trim)))
+
+(defproject storm/storm-console-logging VERSION
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :resource-paths ["logback"]
+  :target-path "target"
+
+  :profiles {:release {}
+             }
+
+  :aot :all)
